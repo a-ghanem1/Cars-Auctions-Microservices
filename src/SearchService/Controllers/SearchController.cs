15ab74e385b4c8efd004f6355457b6243c77008f
Abdelrahman Ghanem <a.ghanem2244@gmail.com>
2025-04-03 15:54:27 +0300

    feat(frontend): implement initial setup and search functionality for the webapp

--- src/SearchService/Controllers/SearchController.cs
+++ src/SearchService/Controllers/SearchController.cs
@@ -21,18 +21,22 @@ public class SearchController : ControllerBase
 
         query = searchParams.OrderBy switch
         {
-            "make" => query.Sort(x => x.Ascending(a => a.Make)),
-            "new" => query.Sort(x => x.Descending(a => a.CreatedAt)),
-            _ => query.Sort(x => x.Ascending(a => a.AuctionEnd)),
+            "make" => query.Sort(x => x.Ascending(x => x.Make)),
+            "new" => query.Sort(x => x.Descending(x => x.CreatedAt)),
+            _ => query.Sort(x => x.Ascending(x => x.AuctionEnd))
         };
 
-        query = searchParams.FilterBy switch
+        if (!string.IsNullOrEmpty(searchParams.FilterBy))
         {
-            "finished" => query.Match(x => x.AuctionEnd < DateTime.UtcNow),
-            "endingSoon" => query.Match(x => x.AuctionEnd < DateTime.UtcNow.AddHours(6) 
-                && x.AuctionEnd > DateTime.UtcNow),
-            _ => query.Match(x => x.AuctionEnd > DateTime.UtcNow)
-        };
+            query = searchParams.FilterBy switch
+            {
+                "finished" => query.Match(x => x.AuctionEnd < DateTime.UtcNow),
+                "endingSoon" => query.Match(x =>
+                    x.AuctionEnd < DateTime.UtcNow.AddHours(6)
+                        && x.AuctionEnd > DateTime.UtcNow),
+                _ => query.Match(x => x.AuctionEnd > DateTime.UtcNow) // live
+            };
+        }
 
         if (!string.IsNullOrEmpty(searchParams.Seller))
         {
